e27a1145c23e269a1f2a9dff813844d793538cd2
Sophie Gu <sophieqgu@gmail.com>
2019-05-23 05:37:11 +0000

    first pry

--- lib/play_GOT/api.rb
+++ lib/play_GOT/api.rb
@@ -1,3 +1,8 @@
 class PlayGOT::API
 
+  def self.list_characters
+    characters = RestClient.get("https://anapioficeandfire.com/api/houses")
+    @characters_list = JSON.parse(characters)
+    binding.pry
+  end 
 end 
--- lib/play_GOT/cli.rb
+++ lib/play_GOT/cli.rb
@@ -2,6 +2,7 @@ class PlayGOT::CLI
   
   def call 
     puts "When you play the Game of Thrones, you win or you die."
+    PlayGOT::API.list_characters
   end 
   
 end 
