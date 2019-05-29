13c8e18ca0f15c82f3b3c7d9cf7b1fdcaf1f2e1b
Sophie Gu <sophieqgu@gmail.com>
2019-05-29 17:30:57 +0000

    updated rules and menu

--- lib/play_GOT/cli.rb
+++ lib/play_GOT/cli.rb
@@ -9,6 +9,10 @@ class PlayGOT::CLI
     PlayGOT::API.new.create_houses
     
     choose_house 
+    
+    rules 
+    
+    menu
   end 
   
   def initiate_player
@@ -29,6 +33,7 @@ class PlayGOT::CLI
     puts "No one doubts it for being the #{@you.house.titles.join(", ").light_green}.\nLet #{@you.house.ancestral_weapons.join(" and ").light_green} be your secret weapon."
     
     puts "\nAre you sure about your choice? Type Y or YES to continue or anything else to choose again.".light_red
+    
     confirm = gets.upcase.strip
       
     if confirm == "Y" || confirm == "YES"
@@ -38,6 +43,48 @@ class PlayGOT::CLI
     end 
   end 
       
+  def rules 
+    puts "\nTo win the Game of Thrones you must do the following: "
+    puts "1 - Survive the Winter that sweeps through Westeros randomly.".blue
+    puts "2 - Conquer all your enemies or turn them into your allies. ".blue
+    puts "Your #{'stamina'.light_green} determines your base rate of surviving the Winter. Each additional ally adds another 10 percent chance."
+    puts "Your #{'tactic'.light_green} determines your base rate of successful fleeing when confronting an enemy."
+    puts "Your #{'loyalty'.light_green} determines your base rate of winning over an ally."
+    puts "\nWhen you are ready, press any key to continue.".light_red
     
+    input = gets.strip
+    
+    menu
+  end 
+  
+  def menu 
+    puts "What do you want to do?".light_red
+    puts "1 - Read the rules again.\n2 - View your current stats.\n3 - Go find an ally.\n4 - Go attack an enemy.\n5 - Read about secret weapons.\n6 - Exit the game"
+    
+    input = gets.strip 
+    
+    case input 
+    when "1" 
+      rules 
+    when "6"
+      exit
+    else   
+      puts "You've spoken something mystical that I don't understand.".light_red
+      menu
+    end 
+  end 
+  
+  def exit 
+    puts "Are you sure you want to leave? Type Y or YES to confirm or anything else to go back to the menu.".light_red
+    
+    confirm = gets.strip.upcase
+    
+    if confirm == "Y" || confirm == "YES"
+      puts "Goodbye, #{@you.name.light_green}. See you next time in the Game of Thrones."
+      exit!
+    else 
+      menu
+    end
+  end 
   
 end 
--- lib/play_GOT/player.rb
+++ lib/play_GOT/player.rb
@@ -10,7 +10,9 @@ class PlayGOT::Player < PlayGOT::Character
   def choose_house
     puts "Choose a number from the list of great houses and swear your allegience: ".light_red 
     PlayGOT::House.list_all
+    
     input = gets.strip.to_i
+    
     if (1..PlayGOT::House.all.size).to_a.include?(input)
       @house = PlayGOT::House.find(input)
     else 
